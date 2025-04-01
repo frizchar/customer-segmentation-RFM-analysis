3b7b746f40867ec3105ce88fc3fec92ccdaa46e9
frizchar <cchrysanth@gmail.com>
2025-04-01 12:49:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     # get processed dataset
     df = process_data.df
 
+    RFMSCORE_THRESHOLD = 8  # min required RFM score to consider a customer as 'high value'
+
     # Example of finding high-value customers (e.g., RFMScore >= 8)
-    high_value_customers = df[df['RFMScore'] >= 8]
+    high_value_customers = df[df['RFMScore'] >= RFMSCORE_THRESHOLD]
     print('high_value_customers :\n', high_value_customers)
+    
