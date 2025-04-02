6a77bb368c00d4dd2288416f362be4e768eda902
frizchar <cchrysanth@gmail.com>
2025-04-02 15:04:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     RFM_score = df.groupby('CustomerID')['RFMScore'].max().reset_index()
     print('RFM score per customer :\n', RFM_score)
 
-    RFMSCORE_THRESHOLD = 8  # min required RFM score to consider a customer as 'high value'
+    RFM_SCORE_THRESHOLD = 8  # min required RFM score to consider a customer as 'high value'
 
-    # Example of finding high-value customers (e.g., RFMScore >= 8)
-    high_value_customers = df[df['RFMScore'] >= RFMSCORE_THRESHOLD]
+    # Example of finding high-value customers (e.g., RFM_score >= 8)
+    high_value_customers = df[df['RFMScore'] >= RFM_SCORE_THRESHOLD]
     print('high_value_customers :\n', high_value_customers[['CustomerID','RFMScore']])
